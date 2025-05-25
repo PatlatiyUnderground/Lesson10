5d95f3a13f277661fca188f9604353c9c457fcee
PatlatiyUnderground <patlatiyunderground@gmail.com>
2025-05-26 00:02:46 +0300

    Lesson10 compleete

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
-            addBook(name);
-            addBook(name, pageCount);
+            int price = inputPrice();
+            addBook(name, pageCount, price);
             //todo использовать новый метод тут
             printInfo();
         }
@@ -25,14 +25,20 @@ public class Main {
         return new Scanner(System.in).nextInt();
     }
 
+    public static int inputPrice() {
+        System.out.println("Введите цену:");
+        return new Scanner(System.in).nextInt();
+    }
+
     public static void addBook(String bookName) {
-        addBook(bookName, 0);
+        addBook(bookName, 0, 0);
     }
 
-    public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+    public static void addBook(String bookName, int pageCount, int price) {
+        booksInfo = booksInfo + bookName +
+                " - " + (pageCount > 0 ? pageCount : "N/A") + " стр." +
+                " - " + (price > 0 ? price : "N/A")+ " р.\n";
     }
-
     public static void printInfo() {
         System.out.println(booksInfo);
     }
